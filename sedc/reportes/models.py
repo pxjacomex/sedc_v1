61e6534d4e34e91e6863fa9724b25c2e2d0914e0
paul19pv <paul_19pv@hotmail.com>
2017-07-13 13:03:11 -0500

    reportes tabla y grafico para cau2012

--- sedc/reportes/models.py
+++ sedc/reportes/models.py
@@ -6,13 +6,3 @@ from django.db.models import Max, Min
 from medicion.models import Medicion
 
 # Create your models here.
-year = str(2012)
-
-if med_fecha in range(year + '-01-01',year + '-01-31'):
-    med_valor__max = Medicion.objects.all().aggregate(Max('med_valor'))
-    med_valor__min = Medicion.objects.all().aggregate(Min('med_valor'))
-    med_valor__med = Medicion.objects.all().aggregate(Median('med_valor'))
-elif med_fecha in range(year + '-02-01',year + '-02-29'):
-    med_valor__max = Medicion.objects.all().aggregate(Max('med_valor'))
-    med_valor__min = Medicion.objects.all().aggregate(Min('med_valor'))
-    med_valor__med = Medicion.objects.all().aggregate(Median('med_valor'))
