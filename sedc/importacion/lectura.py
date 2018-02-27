5890ef96fb49b0ce177ebd170981a1165d12f120
paul19pv <paul_19pv@hotmail.com>
2018-02-27 09:41:37 -0500

    demonio

--- sedc/importacion/lectura.py
+++ sedc/importacion/lectura.py
@@ -6,29 +6,35 @@ from importacion.forms import UploadFileForm
 import daemon
 import time
 def iniciar_lectura():
-    formatos=list(Formato.objects.filter(for_tipo='automatico'))
-    for formato in formatos:
-        consulta=list(Asociacion.objects.filter(for_id=formato.for_id))
-        if len(consulta)>0:
-            estacion=consulta[0].est_id
-            archivo=open(formato.for_ubicacion+formato.for_archivo)
-            datos,variables=procesar_archivo_automatico(archivo,formato,estacion,formato.mar_id)
-            if len(datos)>0:
-                print time.ctime()
-                guardar_datos_automatico(datos,variables)
-                print time.ctime()
-                registro=open('/tmp/sedc.txt','a')
-                registro.write(time.ctime()+': Información guardada '+str(estacion.est_codigo)+str(formato.for_descripcion)+'\n')
-                registro.close()
+    try:
+        formatos=list(Formato.objects.filter(for_tipo='automatico'))
+        for formato in formatos:
+            consulta=list(Asociacion.objects.filter(for_id=formato.for_id))
+            if len(consulta)>0:
+                estacion=consulta[0].est_id
+                archivo=open(formato.for_ubicacion+formato.for_archivo)
+                datos,variables=procesar_archivo_automatico(archivo,formato,estacion,formato.mar_id)
+                archivo.close()
+                if len(datos)>0:
+                    guardar_datos_automatico(datos,variables)
+                    registro=open('/tmp/sedc.txt','a')
+                    registro.write(time.ctime()+': Información guardada '+str(estacion.est_codigo)+str(formato.for_descripcion)+'\n')
+                    registro.close()
 
+                else:
+                    registro=open('/tmp/sedc.txt','a')
+                    registro.write(time.ctime()+': No existe nueva informacion para el formato '+str(formato.for_descripcion)+'\n')
+                    registro.close()
             else:
                 registro=open('/tmp/sedc.txt','a')
-                registro.write(time.ctime()+': No existe nueva informacion'+'\n')
+                registro.write(time.ctime()+': No existen formatos para iniciar la lectura'+'\n')
                 registro.close()
-        else:
-            registro=open('/tmp/sedc.txt','a')
-            registro.write(time.ctime()+': No existen formatos para iniciar la lectura'+'\n')
-            registro.close()
-            break
+                break
+    except IOError as e:
+        registro=open('/tmp/sedc.txt','a')
+        registro.write(time.ctime()+': El archivo no existe'+'\n')
+        registro.close()
+        pass
+    #time.sleep(900)
 
             #guardar_datos(datos,variable)
