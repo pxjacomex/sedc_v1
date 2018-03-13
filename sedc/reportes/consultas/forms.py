811ec75aa31f4cfc68b442e49f22d1f0a98b7e22
paul19pv <paul_19pv@hotmail.com>
2018-03-12 19:24:27 -0500

    correcion del reporte de datos

--- sedc/reportes/consultas/forms.py
+++ sedc/reportes/consultas/forms.py
@@ -33,9 +33,10 @@ class MedicionSearchForm(forms.Form):
         queryset=Estacion.objects.order_by('est_id').all())
     variable=forms.ModelChoiceField(
         queryset=Variable.objects.order_by('var_id').all())
-    frecuencia=forms.ChoiceField(choices=FRECUENCIA)
+
     inicio=forms.DateField(input_formats=['%d/%m/%Y'],label="Fecha de Inicio(dd/mm/yyyy)")
     fin=forms.DateField(input_formats=['%d/%m/%Y'],label="Fecha de Fin(dd/mm/yyyy)")
+    frecuencia=forms.ChoiceField(choices=FRECUENCIA)
     #saber si hay datos
     def exists(self,form):
         #print self.estacion
